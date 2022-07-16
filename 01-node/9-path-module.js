cd497e959ccb934570bf018fcd13fe32e43f9e97
Aashish84 <aashishkr.thapa84@gmail.com>
2022-07-16 22:08:51 +0545

    gitignore added , path built in module demo

--- 01-node/9-path-module.js
+++ 01-node/9-path-module.js
@@ -5,12 +5,17 @@ const path = require("path");
 //path seperator like in this path D:\node_js\01-node -> \ sign for window -> / for mac
 console.log(path.sep);
 
-const filepath = path.join(`${path.sep}content`, "subfolder", "test.txt");
-console.log(filepath);
-console.log(path.basename(filepath));
+const filepath = path.join(
+  __dirname,
+  `${path.sep}content`,
+  "subfolder",
+  "test.txt"
+);
+console.log("filepath : " + filepath);
+console.log("basename : " + path.basename(filepath));
 
 // relolve - Resolves the specified paths into an absolute path
 // https://stackoverflow.com/questions/31317007/get-full-file-path-in-node-js
 
 const absolute = path.resolve(__dirname, "content", "subfolder", "test.txt");
-console.log(absolute);
+console.log("resolve demo : " + absolute);
