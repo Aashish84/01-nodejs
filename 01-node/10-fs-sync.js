e9838aa5dfaa0004741d09f7efed7d87b733d521
Aashish84 <aashishkr.thapa84@gmail.com>
2022-07-17 17:20:33 +0545

    async file handeling

---
+++ 01-node/10-fs-sync.js
@@ -0,0 +1,20 @@
+const { resolve } = require("path");
+const { readFileSync, writeFileSync } = require("fs");
+
+const counter_file_path = resolve(__dirname, "content", "counter.txt");
+const count_value = readFileSync(counter_file_path, "utf-8");
+
+const first = readFileSync("./content/first.txt", "utf8");
+const second = readFileSync("./content/second.txt", "utf8");
+
+console.log(first);
+console.log(second, first);
+
+writeFileSync(
+  "./content/sync-demo.txt",
+  `${count_value} ## file first : ${first} and second file : ${second}\n`,
+  { flag: "a" }
+);
+
+const new_count_value = parseInt(count_value) + 1;
+writeFileSync(counter_file_path, `${new_count_value}`);
