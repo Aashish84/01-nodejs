3644fd01729eeb657ca8564117a17a65dfeac524
Aashish84 <aashishkr.thapa84@gmail.com>
2022-09-17 13:12:25 +0545

    product-crud
    basic crud operation for product collection

--- node-practise/JWT-role-based/controller/product.js
+++ node-practise/JWT-role-based/controller/product.js
@@ -1,18 +1,42 @@
-const getAllProduct = (req, res) => {
-  res.send("hello from getAllProduct");
+const Product = require("../models/Product");
+const { NotFoundError } = require("../errors");
+
+const getAllProduct = async (req, res) => {
+  const products = await Product.find({});
+  res.status(200).json(products);
 };
-const getProduct = (req, res) => {
-  res.send(req.params);
+
+const getProduct = async (req, res) => {
+  const { id: productID } = req.params;
+  const product = await Product.findOne({ _id: productID });
+  if (!product) {
+    throw new NotFoundError("no product of id found to display");
+  }
+  res.status(200).json(product);
 };
 
 const addProduct = async (req, res) => {
-  res.send("hello from addproduct");
+  const product = await Product.create(req.body);
+  res.status(201).json(product);
 };
+
 const updateProduct = async (req, res) => {
-  res.send(req.params);
+  const { id: productID } = req.params;
+  const product = await Product.findOneAndUpdate({ _id: productID }, req.body, {
+    new: true,
+  });
+  if (!product) {
+    throw new NotFoundError("no product found of given id to update");
+  }
+  res.status(200).json(product);
 };
 const deleteProduct = async (req, res) => {
-  res.send("delete product");
+  const { id: productID } = req.params;
+  const product = await Product.findOneAndDelete({ _id: productID });
+  if (!product) {
+    throw new NotFoundError("no product found of given id to delete");
+  }
+  res.status(200).json(product);
 };
 
 module.exports = {
--- node-practise/JWT-role-based/models/Product.js
+++ node-practise/JWT-role-based/models/Product.js
@@ -1,1 +1,30 @@
 const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "no product name"],
+      minLength: 4,
+    },
+    price: {
+      type: Number,
+      required: [true, "no price for product"],
+    },
+    discount: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["Vegetables", "Fruits"],
+        message: "{VALUE} is not supported for product category",
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
